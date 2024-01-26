7c7ab3502e07bd21b1bd60bbc0bd722cdfb3eea0
duongbuibd <duongbuibd@gmail.com>
2024-01-26 20:44:26 +0700

    config

--- client/src/app/api/apiSlice.ts
+++ client/src/app/api/apiSlice.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:5000/api/v1",
+    baseUrl: "https://dashboard.render.com//api/v1",
   }),
   tagTypes: ["Products", "Users"],
   endpoints: (builder) => ({}),
